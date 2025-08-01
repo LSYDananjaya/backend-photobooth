8e273625f302a5ae6d76c4a4418bd26383b9229c
Yehara Dananjaya <yeharadananjaya@gmail.com>
2025-08-01 11:17:48 +0530

    Add allow_unsafe_werkzeug to socketio.run
    Enables the allow_unsafe_werkzeug option when running the app with socketio. This may be required for compatibility with recent versions of Flask-SocketIO or Werkzeug.

--- src/main.py
+++ src/main.py
@@ -285,5 +285,5 @@ def serve(path):
             return "index.html not found", 404
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
 
